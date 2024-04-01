699c21ec0a6f3265c120a00fa005399b5015c3eb
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2024-04-01 17:50:32 +0400

    drag n drop

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import './assets/main.css'
+import './assets/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 // Vuetify
-import 'vuetify/styles'
+import "vuetify/styles";
+import "@/styles/main.scss";
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { plugin as Slicksort } from 'vue-slicksort';
 
 const vuetify = createVuetify({
     components,
     directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(Slicksort).mount('#app')
